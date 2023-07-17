e650be11492c8ae849efa6df61ac108150ae8d0b
Mars <almasnorazman2803@outlook.com>
2023-07-17 18:28:38 +0800

    No one likes making test files

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnorazma <nnorazma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/17 18:28:12 by nnorazma          #+#    #+#             */
+/*   Updated: 2023/07/17 18:28:13 by nnorazma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <stack>
+
+template<typename T> class MutantStack : public std::stack<T> {
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		MutantStack( void ) {
+			std::cout << "MutantStack constructed." << std::endl;
+		}
+
+		MutantStack( const MutantStack &copy ) {
+			*this = copy;
+			std::cout << "MutantStack cloned." << std::endl;
+		}
+
+		MutantStack<T> &operator=( const MutantStack<T> &assign ) {
+			this = assign;
+			return (*this);
+		}
+
+		~MutantStack( void ) {
+			std::cout << "MutantStack destructed." << std::endl;
+		}
+
+		/*
+		Stack has an underlying container, default is deque.
+		But we can set it to any other container we want.
+		So we can't access the stack with iterators, but we can
+		access the underlying containers.
+		?????
+		*/
+		iterator begin( void ) {
+			return (this->c.begin());
+		}
+
+		iterator end( void ) {
+			return (this->c.end());
+		}
+};
---
+++ ex02/main.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnorazma <nnorazma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/17 18:28:08 by nnorazma          #+#    #+#             */
+/*   Updated: 2023/07/17 18:28:09 by nnorazma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
